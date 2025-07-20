a71ee9e0cd9f0e47eef43048e73ea91a6addd889
Watuulo-Richard <watuulorichard@gmail.com>
2025-07-20 14:40:43 +0300

    Update hero section title
    - Edited the title in the hero section to better reflect Kiseeka Pius's professional role and enhance the portfolio's presentation.

--- components/frontend/hero.tsx
+++ components/frontend/hero.tsx
@@ -18,7 +18,7 @@ export default function Hero({
     text: "I'm ",
   },
   {
-    text: `${fetchedProfile.title}`,
+    text: `${fetchedProfile.title.split(' ').join('-')}`,
     className: "bg-gradient-to-r from-primary to-primary/70 bg-clip-text text-transparent",
   },
   {
