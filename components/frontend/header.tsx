3203914b8deee9c03e8dec353ee8b3b9e502960b
Watuulo-Richard <watuulorichard@gmail.com>
2025-12-30 19:52:46 +0300

    add: new images to gallery

--- components/frontend/header.tsx
+++ components/frontend/header.tsx
@@ -17,7 +17,6 @@ const navItems = [
   { name: "Home", href: "#home" },
   { name: "About", href: "#about" },
   { name: "Experience", href: "#experience" },
-  { name: "Projects", href: "#projects" },
   { name: "Education", href: "#education" },
   { name: "Blog", href: "#blog" },
   { name: "Contact Me", href: "#contact" },
